9e0b44d0b68cb5b678eb0f7af9508d217abb059c
RJ MacReady <82rjmacready@gmail.com>
2013-07-14 02:58:56 -0700

    one more chart

--- gnuplot-server/query.lisp
+++ gnuplot-server/query.lisp
@@ -79,22 +79,26 @@ order by sumSelfCost desc ;"))) ;  limit 25
 	 ;(print `(trace ,trace))
 	 (let ((traceid (car trace)))
 	   (with-open-stream (*standard-output* (open (format nil "out_mem.dat" traceid) :direction :output :if-exists :supersede))
-	     (let ((*c* 0))
+	     (let ((*c* 0)
+		   (i 1))
 	       (loop 
 		  for x from 0 by 10000 
 		  do (progn
 		       (setf *c* 0)
 		       (let ((rows (clsql:query
-				    (format nil "SELECT traceid, id, timeoffset, memory FROM tracelinestore WHERE traceid = ~a limit ~a, 10000;" traceid x))))
+				    (format nil "SELECT traceid, id, timeoffset, memory, level FROM tracelinestore WHERE traceid = ~a limit ~a, 10000;" traceid x))))
 			 (mapc 
 			  (lambda (row)
 			    (setf *c* (1+ *c*))
+			    (setf i (1+ i))
 			    (write-string 
-			     (format nil "~a ~a ~f ~a ~a" 
+			     (format nil "~a ~a ~f ~a ~a ~a ~a" 
 				     (nth 0 row)
 				     (nth 1 row)
 				     (nth 2 row)
 				     (nth 3 row)
+				     i
+				     (nth 4 row)
 				     #\Newline)))
 			  rows)
 			 nil))
