3604e662ad0a2dc4d73bdbda831c950db3b4d350
RJ MacReady <82rjmacready@gmail.com>
2013-08-18 11:59:44 -0700

    some cool stuff working

---
+++ views/v2/test.lisp
@@ -0,0 +1,46 @@
+
+(defparameter *tree* nil)
+; init tree. 
+; ideally, this html could be compiled from a .html file
+(setf *tree* (fromwho '(:html 
+			(:head
+			 (:title)) 
+			(:body 
+			 (:h1 "test")
+			 (:div :id "header")
+			 (:div :id "container")
+			 (:div)
+			 (:div :id "footer")))))
+
+; lets walk the htmlnode tree, print ourselves some tags
+(walk (lambda (node)
+	(with-slots (tagname attrs) node
+	  (print `(,tagname ,attrs)))) *tree*)
+
+; create instance of lquery (analogous to $)
+(defparameter *lq* nil)
+(setf *lq* (make-instance 'lquery :tree *tree*))
+
+(defparameter *res* nil)
+
+; dump tree
+(towho *tree*)
+
+; change content of <title>
+; $('title').html('hello world');
+(setf *res* (select *lq* (make-instance 'selector-tagname
+					:tagname :title)))
+(html *res* "hello world")
+
+; change content of div#container
+; $('#container').html('<div>...</div><div>...</div>');
+(setf *res* (select *lq* (make-instance 'selector-attr
+					:attr-name :id 
+					:attr-value "container")))
+
+(html *res* 
+      '(:div "added dynamically") 
+      '(:div "so was this one"))
+
+; dump tree
+(towho *tree*)
